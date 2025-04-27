6f4bb9c7770d2fe4aff476b133f0c8050165a13e
sppucode <sppupractical7@gmail.com>
2025-04-27 22:18:29 +0530

    Update and rename Special_Expert.py to Expert.py

--- Expert.py
+++ Expert.py
@@ -3,27 +3,26 @@ disease_symptoms = {
     "Flu": ["fever", "cough", "throat pain", "body pain", "tired"],
     "Cold": ["sneeze", "runny nose", "throat pain", "mild cough", "headache"],
     "Malaria": ["fever", "chills", "sweating", "nausea", "vomit"],
-    "COVID-19": ["fever", "dry cough", "loss of taste", "breathless", "tired"],
     "Dengue": ["high fever", "headache", "eye pain", "joint pain", "rash"],
     "Typhoid": ["fever", "stomach pain", "weakness", "no hunger", "loose motion"],
     "Asthma": ["wheezing", "breathless", "chest tight", "cough"],
-    "Pneumonia": ["chest pain", "breathless", "fever", "cough with mucus", "tired"]
 }
 
 disease_treatments = {
     "Flu": "Rest, drink water, and take medicines if needed.",
     "Cold": "Stay warm, drink fluids, and use cold syrup.",
     "Malaria": "Visit doctor and take malaria medicines.",
-    "COVID-19": "Stay isolated and consult doctor.",
     "Dengue": "Rest, drink water, avoid aspirin.",
     "Typhoid": "Use antibiotics and drink water.",
     "Asthma": "Use inhaler and avoid dust.",
-    "Pneumonia": "See doctor, take antibiotics, and rest."
 }
 
 # Get symptoms from user
-user_input = input("Enter at least 4 symptoms separated by commas: ").lower()
-user_symptoms = [s.strip() for s in user_input.split(",")]
+user_symptoms =[]
+print("Enter 4 symptoms")
+for i in range(4):
+    user_input = input("Enter symptoms").lower()
+    user_symptoms.append(user_input)
 
 # Check if user entered enough symptoms
 if len(user_symptoms) < 4:
@@ -46,8 +45,8 @@ else:
     if sorted_diseases:
         print("\nDiagnosis Probabilities:")
         for disease, probability in sorted_diseases:
-            print(f"\n➔ Disease: {disease}")
-            print(f"   ➔ Probability: {probability:.2f}%")
-            print(f"   ➔ Treatment: {disease_treatments[disease]}")
+            print(f"\n-> Disease: {disease}")
+            print(f"   -> Probability: {probability:.2f}%")
+            print(f"   -> Treatment: {disease_treatments[disease]}")
     else:
         print("\nNo matching disease found. Please consult a doctor.")
